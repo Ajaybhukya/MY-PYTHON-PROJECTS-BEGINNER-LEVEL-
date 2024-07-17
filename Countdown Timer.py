d023f760121569551fc6569fc04380f6fc6ce9dd
Ajaybhukya <158462635+Ajaybhukya@users.noreply.github.com>
2024-07-17 11:50:35 +0530

    Add files via upload

---
+++ Countdown Timer.py
@@ -0,0 +1,18 @@
+"""
+try to rin the code in the jupyter notebook which gives you the best output
+"""
+import time
+def count(seconds):
+    while seconds:
+        mins,secs=divmod(seconds,60)
+        timer='{:02d}:{:02d}'.format(mins,secs)
+        print(timer,end='\r')
+        time.sleep(1)
+        seconds-=1
+    print("Time out")
+try:
+    secons=int(input("Enter the seconds:"))
+    count(secons)
+except ValueError as e:
+    print("Invalid input")
+    print("Program exited")
